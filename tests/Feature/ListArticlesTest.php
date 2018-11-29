897d6e0a05dcd818cc72f13b5aa989843bba920e
Mattia Basone <mattia.basone@vidiemme.it>
2018-11-29 14:46:11 +0100

    feat(article): Add ListArticleController

---
+++ tests/Feature/ListArticlesTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class ListArticlesTest extends TestCase
+{
+    /**
+     * List articles test
+     * I metodi nella classe test devono iniziare con la parola "test" o non vengono interpretati da phpunit
+     * In alternativa si può usare l'annotation @test.
+     *
+     * @test
+     */
+    public function shouldListArticles()
+    {
+        $response = $this->get('api/articles');
+        $response->assertStatus(200);
+        $response->assertJson([]);
+    }
+}
